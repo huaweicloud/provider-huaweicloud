c085ea9c6f84cc551c51884c5877e177337a8f4b
Jason Zhang <jasonzhang9309@163.com>
2025-11-12 16:25:19 +0800

    support more arguments in provider config

--- internal/clients/huaweicloud.go
+++ internal/clients/huaweicloud.go
@@ -27,9 +27,42 @@ const (
 	errUnmarshalCredentials = "cannot unmarshal huaweicloud credentials as JSON"
 
 	// provider config
-	keyRegion    = "region"
-	keyAccessKey = "access_key"
-	keySecretKey = "secret_key"
+	keyRegion               = "region"
+	keyAccessKey            = "access_key"
+	keySecretKey            = "secret_key"
+	keySecurityToken        = "security_token"
+	keyDomainID             = "domain_id"
+	keyDomainName           = "domain_name"
+	keyUserName             = "user_name"
+	keyUserID               = "user_id"
+	keyPassword             = "password"
+	keyAssumeRole           = "assume_role"
+	keyAssumeRoleWithOIDC   = "assume_role_with_oidc"
+	keyProjectID            = "project_id"
+	keyProjectName          = "project_name"
+	keyTenantID             = "tenant_id"
+	keyTenantName           = "tenant_name"
+	keyToken                = "token"
+	keyInsecure             = "insecure"
+	keyCACertFile           = "cacert_file"
+	keyCert                 = "cert"
+	keyKey                  = "key"
+	keyAgencyName           = "agency_name"
+	keyAgencyDomainName     = "agency_domain_name"
+	keyDelegatedProject     = "delegated_project"
+	keyAuthURL              = "auth_url"
+	keyCloud                = "cloud"
+	keyEndpoints            = "endpoints"
+	keyRegional             = "regional"
+	keySharedConfigFile     = "shared_config_file"
+	keyProfile              = "profile"
+	keyEnterpriseProjectID  = "enterprise_project_id"
+	keyMaxRetries           = "max_retries"
+	keyEnableForceNew       = "enable_force_new"
+	keySigningAlgorithm     = "signing_algorithm"
+	keySkipCheckWebsiteType = "skip_check_website_type"
+	keySkipCheckUpgrade     = "skip_check_upgrade"
+	keyDefaultTags          = "default_tags"
 )
 
 // TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
@@ -62,7 +95,7 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 		if err != nil {
 			return ps, errors.Wrap(err, errExtractCredentials)
 		}
-		creds := map[string]string{}
+		creds := map[string]interface{}{}
 		if err := json.Unmarshal(data, &creds); err != nil {
 			return ps, errors.Wrap(err, errUnmarshalCredentials)
 		}
@@ -77,6 +110,105 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 		}
 		if v, ok := creds[keySecretKey]; ok {
 			ps.Configuration[keySecretKey] = v
+		}
+		if v, ok := creds[keySecurityToken]; ok {
+			ps.Configuration[keySecurityToken] = v
+		}
+		if v, ok := creds[keyDomainID]; ok {
+			ps.Configuration[keyDomainID] = v
+		}
+		if v, ok := creds[keyDomainName]; ok {
+			ps.Configuration[keyDomainName] = v
+		}
+		if v, ok := creds[keyUserName]; ok {
+			ps.Configuration[keyUserName] = v
+		}
+		if v, ok := creds[keyUserID]; ok {
+			ps.Configuration[keyUserID] = v
+		}
+		if v, ok := creds[keyPassword]; ok {
+			ps.Configuration[keyPassword] = v
+		}
+		if v, ok := creds[keyAssumeRole]; ok {
+			ps.Configuration[keyAssumeRole] = v
+		}
+		if v, ok := creds[keyAssumeRoleWithOIDC]; ok {
+			ps.Configuration[keyAssumeRoleWithOIDC] = v
+		}
+		if v, ok := creds[keyProjectID]; ok {
+			ps.Configuration[keyProjectID] = v
+		}
+		if v, ok := creds[keyProjectName]; ok {
+			ps.Configuration[keyProjectName] = v
+		}
+		if v, ok := creds[keyTenantID]; ok {
+			ps.Configuration[keyTenantID] = v
+		}
+		if v, ok := creds[keyTenantName]; ok {
+			ps.Configuration[keyTenantName] = v
+		}
+		if v, ok := creds[keyToken]; ok {
+			ps.Configuration[keyToken] = v
+		}
+		if v, ok := creds[keyInsecure]; ok {
+			ps.Configuration[keyInsecure] = v
+		}
+		if v, ok := creds[keyCACertFile]; ok {
+			ps.Configuration[keyCACertFile] = v
+		}
+		if v, ok := creds[keyCert]; ok {
+			ps.Configuration[keyCert] = v
+		}
+		if v, ok := creds[keyKey]; ok {
+			ps.Configuration[keyKey] = v
+		}
+		if v, ok := creds[keyAgencyName]; ok {
+			ps.Configuration[keyAgencyName] = v
+		}
+		if v, ok := creds[keyAgencyDomainName]; ok {
+			ps.Configuration[keyAgencyDomainName] = v
+		}
+		if v, ok := creds[keyDelegatedProject]; ok {
+			ps.Configuration[keyDelegatedProject] = v
+		}
+		if v, ok := creds[keyAuthURL]; ok {
+			ps.Configuration[keyAuthURL] = v
+		}
+		if v, ok := creds[keyCloud]; ok {
+			ps.Configuration[keyCloud] = v
+		}
+		if v, ok := creds[keyEndpoints]; ok {
+			ps.Configuration[keyEndpoints] = v
+		}
+		if v, ok := creds[keyRegional]; ok {
+			ps.Configuration[keyRegional] = v
+		}
+		if v, ok := creds[keySharedConfigFile]; ok {
+			ps.Configuration[keySharedConfigFile] = v
+		}
+		if v, ok := creds[keyProfile]; ok {
+			ps.Configuration[keyProfile] = v
+		}
+		if v, ok := creds[keyEnterpriseProjectID]; ok {
+			ps.Configuration[keyEnterpriseProjectID] = v
+		}
+		if v, ok := creds[keyMaxRetries]; ok {
+			ps.Configuration[keyMaxRetries] = v
+		}
+		if v, ok := creds[keyEnableForceNew]; ok {
+			ps.Configuration[keyEnableForceNew] = v
+		}
+		if v, ok := creds[keySigningAlgorithm]; ok {
+			ps.Configuration[keySigningAlgorithm] = v
+		}
+		if v, ok := creds[keySkipCheckWebsiteType]; ok {
+			ps.Configuration[keySkipCheckWebsiteType] = v
+		}
+		if v, ok := creds[keySkipCheckUpgrade]; ok {
+			ps.Configuration[keySkipCheckUpgrade] = v
+		}
+		if v, ok := creds[keyDefaultTags]; ok {
+			ps.Configuration[keyDefaultTags] = v
 		}
 		return ps, nil
 	}
